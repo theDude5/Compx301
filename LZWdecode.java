154082fca93fa6ebf1b1a8977aaea0e4bbe4e2d9
ryangood1 <62087744+ryangood1@users.noreply.github.com>
2020-03-17 12:54:05 +1300

    Add files via upload
    Initial start of the decoder

--- LZWdecode.java
+++ LZWdecode.java
@@ -1,5 +1,24 @@
-class LZWdecode {
+import java.util.ArrayList;
+
+public class LZWdecode {
+
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		byte data = 0; 		//The incoming compressed byte data to be decoded
+		int initialDicLength = 256;		//The initial size of the dictionary
+		int phraseNum = 0;		//The Phrase number
+		char c;		//The decoded char to be outputed
+		char nextChar;		//Char to hold the unknown value of the next char
+		
+
+		c = (char)data;
+
+		ArrayList<String> dictionary = new ArrayList<String>();
+		dictionary.add("a");
+		dictionary.add("b");
+		dictionary.add("c");
+		for(int i = 0; i < dictionary.size(); i++) {
+			System.out.println(dictionary.get(i));
+		}
+		System.out.println("End of Code");
 	}
 }
