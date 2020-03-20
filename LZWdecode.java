d80d0b0b69c27783d5d65e14bcbe7e191a36aa47
ryangood1 <62087744+ryangood1@users.noreply.github.com>
2020-03-20 13:53:17 +1300

    Add files via upload

--- LZWdecode.java
+++ LZWdecode.java
@@ -1,24 +1,44 @@
-import java.util.ArrayList;
+import java.io.*; 
+import java.util.*; 
 
 public class LZWdecode {
 
 	public static void main(String[] args) {
 		byte data = 0; 		//The incoming compressed byte data to be decoded
-		int initialDicLength = 256;		//The initial size of the dictionary
+		int dictLength = 256;		//The initial size of the dictionary
 		int phraseNum = 0;		//The Phrase number
 		char c;		//The decoded char to be outputed
 		char nextChar;		//Char to hold the unknown value of the next char
 		
-
 		c = (char)data;
 
-		ArrayList<String> dictionary = new ArrayList<String>();
-		dictionary.add("a");
-		dictionary.add("b");
-		dictionary.add("c");
-		for(int i = 0; i < dictionary.size(); i++) {
-			System.out.println(dictionary.get(i));
-		}
-		System.out.println("End of Code");
+		Stack<Integer> stack = new Stack<Integer>(); 
+	
+		ArrayList<Integer> phraseNumber = new ArrayList<Integer>();
+		ArrayList<String> character = new ArrayList<String>();
 	}
+
+	public void decode(byte data) {
+		
+	}
+
+	static void stack_push(Stack<Integer> stack) 
+    { 
+        for(int i = 0; i < 5; i++) 
+        { 
+            stack.push(i); 
+        } 
+    } 
+      
+    // Popping element from the top of the stack 
+    static void stack_pop(Stack<Integer> stack) 
+    { 
+        System.out.println("Pop :"); 
+  
+        for(int i = 0; i < 5; i++) 
+        { 
+            Integer y = (Integer)stack.pop(); 
+            System.out.println(y); 
+        } 
+    } 
 }
