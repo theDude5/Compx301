32faa5ebae53939fd71302a13182163b9195601e
theDude5 <22064477+theDude5@users.noreply.github.com>
2020-03-31 14:45:43 +1300

    Update Encoder.java

--- Encoder.java
+++ Encoder.java
@@ -5,22 +5,21 @@
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.FileNotFoundException;
  
-public class Encoder{
-    private final int char_space = (int) Math.pow(2, 8); // character space
-    private final Node[] root; // trie root
+class Encoder{
+    private final int DOMAIN = (int) Math.pow(2, 8); // character space
+    private final Node[] ROOT; // trie ROOT
     private Node pointer;
     private int count; // phrase counter
-    
+
     private class Node {
         int rank; // phrase number
         byte key; // Symbol
         ArrayList<Node> search_space = new ArrayList<Node>(); // child nodes
-        ArrayList<Byte> index = new ArrayList<Byte>(); // indexed of nodes 
+        ArrayList<Byte> index = new ArrayList<Byte>(); // index of node indexes
         
         private Node(int rank, byte key) {
             this.rank = rank;
@@ -33,18 +32,18 @@ public class Encoder{
                 pointer = search_space.get(temp);
                 return -1;
             }
-            else { // add new node, index node and go back to root
+            else { // add new node, index node and go back to ROOT
                 search_space.add(new Node(count++ ,value));
                 index.add(value);
-                pointer = root[value]; // start new phrase beginning with unmatched value
+                pointer = ROOT[Math.abs(value)]; // start new phrase beginning with unmatched value
                 return rank;
             }
         }
     }
 
-    private Encoder () {
-        root = new Node[char_space];
-        for (count = 0; count < root.length; count++) { root[count] = new Node(count, (byte) count); }
+    public Encoder() {
+        ROOT = new Node[DOMAIN];
+        for (count = 0; count < ROOT.length; count++) { ROOT[count] = new Node(count, (byte) count); }
     }
     
     /**
@@ -55,12 +54,12 @@ public class Encoder{
     public void encode(File file) throws IOException {
         FileInputStream in = new FileInputStream(file);
         if (in.available() > 0) {
-            File output = new File(file.getPath().substring(0,file.getPath().indexOf('.'))+".lzw");
+            File output = new File(file.getPath().substring(0,file.getPath().indexOf('.'))+".csv");
             output.createNewFile();
             FileWriter out = new FileWriter(output);
             byte value;
             int result;
-            pointer = root[(byte) in.read()];
+            pointer = ROOT[(byte) in.read()];
             while (in.available() > 0) {
                 value = (byte) in.read();
                 result = pointer.query(value);
@@ -85,7 +84,7 @@ public class Encoder{
     public static void main(String[] args) throws FileNotFoundException, IOException { 
         if (args.length == 0){ 
             System.out.println("Usage: java <filepath>");
-            args = new String[]{"tests/test1.txt"};
+            args = new String[]{"tests/MobyDick.txt"};
         }
         File file = new File(args[0]);
         if (!file.exists() || file.length() == 0) { System.out.println("File is empty or does not exist"); }
