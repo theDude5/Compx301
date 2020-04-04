54a902fc581e87c99f2194e109a849fc201bf2a5
ryangood1 <62087744+ryangood1@users.noreply.github.com>
2020-04-05 11:36:47 +1200

    Add files via upload

--- LZWdecode.java
+++ LZWdecode.java
@@ -1,44 +1,43 @@
 import java.io.*; 
 import java.util.*; 
 
-public class LZWdecode {
+//Author: Ryan Good ID:1353453
 
-	public static void main(String[] args) {
-		byte data = 0; 		//The incoming compressed byte data to be decoded
-		int dictLength = 256;		//The initial size of the dictionary
-		int phraseNum = 0;		//The Phrase number
-		char c;		//The decoded char to be outputed
-		char nextChar;		//Char to hold the unknown value of the next char
-		
-		c = (char)data;
+class LZWdecode {
+    phrase[] dictionary;
 
-		Stack<Integer> stack = new Stack<Integer>(); 
-	
-		ArrayList<Integer> phraseNumber = new ArrayList<Integer>();
-		ArrayList<String> character = new ArrayList<String>();
+    private class phrase {
+        int phraseNumber;
+        String symbol;
+
+        private void addItem(int _phraseNumber, String _symbol) {
+            phraseNumber = _phraseNumber;
+            symbol = _symbol;
+        }
+
+        private void setVars(int phrase, String symb) {
+            phraseNumber = phrase;
+            symbol = symb;
+        }
+
+        private void output() {
+            System.out.print("Phrase Number: "  + phraseNumber + ", " + "Symbol: " + symbol);
+        }
+    }
+
+	public LZWdecode() {
+		dictionary  = new phrase[256];
+        phrase c = new phrase();
+        c.setVars(1, "A");
+        c.output();
 	}
 
-	public void decode(byte data) {
-		
-	}
+	public void decode() {
+        System.out.println("Decode bytes and output to file");
+    }
 
-	static void stack_push(Stack<Integer> stack) 
-    { 
-        for(int i = 0; i < 5; i++) 
-        { 
-            stack.push(i); 
-        } 
-    } 
-      
-    // Popping element from the top of the stack 
-    static void stack_pop(Stack<Integer> stack) 
-    { 
-        System.out.println("Pop :"); 
-  
-        for(int i = 0; i < 5; i++) 
-        { 
-            Integer y = (Integer)stack.pop(); 
-            System.out.println(y); 
-        } 
-    } 
+    public static void main(String[] args) {
+        LZWdecode decoder = new LZWdecode();
+        decoder.decode();
+    }
 }
