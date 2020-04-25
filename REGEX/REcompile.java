60ccd4e663bd62567e6919b7f555c5a89189d145
theDude5 <22064477+theDude5@users.noreply.github.com>
2020-04-25 12:42:39 +1200

    Update REcompile.java

--- REGEX/REcompile.java
+++ REGEX/REcompile.java
@@ -29,19 +29,19 @@ public class REcompile {
         if (i >= regex.length) { return; }
         if (regex[i] == '*') { states[state-1].n2 = r; i++; }
         else if (regex[i] == '?') {
-            if (states[state-1].ch != '\0'){r--;}
+            if (states[state-1].ch != '\0') { r--; }
             states[r].n2 = state;
             i++; 
         }
         else if (regex[i] == '|') {
             if (states[state-1].ch != '\0') { states[state] = new State('\0', state+1); }
             states[state-1].n2 = r;
-            if (states[r-1].n1 == states[r-1].n2){ states[r-1].n2 = state-1;}
+            if (states[r-1].n1 == states[r-1].n2) { states[r-1].n2 = state-1; }
             states[r-1].n1 = state-1;
-            i++;
+            r = s; i++;
             term();
-            if (states[r].n1 == states[r].n2){ states[r].n2 = state; }
-            states[r].n1 = state;
+            if (states[r].n1 == states[r].n2) { states[r].n2 = state+1; }
+            states[r].n1 = state+1;
         }
     }
 
@@ -52,20 +52,20 @@ public class REcompile {
         if ("*?|".contains(regex[i]+"")) { throw new Error("Misplaced operator"); }
         switch (regex[i]) {
             case '\\':
+                s = state;
                 states[state] = new State(regex[i+1], state+1);
                 i+=2;
                 break;
-            case '(':    
+            case '(':
                 states[state] = new State('\0', state+1);
                 int r = state-1; i++;
                 expression();
-                if (i >= regex.length || regex[i] != ')') { throw new Error("Missing ')'"); }      
-                states[state] = new State('\0', state+1);
+                if (i >= regex.length || regex[i] != ')') { throw new Error("Missing ')'"); }
                 i++;
                 return r;
             case ')': break;
-            // case '.': s = i; states[state] = new State('.', state+1); i++; break;
-            default: s = i; states[state] = new State(regex[i], state+1); i++;
+            // case '.': s = state; states[state] = new State('.', state+1); i++; break;
+            default: s = state; states[state] = new State(regex[i], state+1); i++;
         }
         return state-1;
     }
@@ -76,14 +76,16 @@ public class REcompile {
         if (i != regex.length){ throw new Error("Illegal Expression"); }
         print();
     }
+
     public void print() {
         System.out.println("state\tch\tn1\tn2\n---------------------------");
         for (State c: states) { if (c != null) { System.out.println(c.st+"\t"+c.ch+"\t"+c.n1+"\t"+c.n2); }}
     }
+
     public static void main(String[] args) {
         if (args.length == 0 || !args[0].startsWith("\"") || !args[0].endsWith("\"")) { 
             System.out.println("Usage: java REcompile \\\" <expression> \\\"");
-            args = new String[] {"\""+".(a|c|(b?)|C*)?"+"\""};
+            args = new String[] {"\""+".(\\?(c?)|((mb)?)|Cm*)?"+"\""};
             // return;
         }
         try { REcompile RE = new REcompile(args[0]); } catch (Error e) { System.err.println(e); }
