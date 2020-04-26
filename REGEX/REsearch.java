3ebc903f3b5c207dfea56771ac87cda04d8290ea
ryangood1 <62087744+ryangood1@users.noreply.github.com>
2020-04-26 13:22:41 +1200

    Add files via upload

--- REGEX/REsearch.java
+++ REGEX/REsearch.java
@@ -1,25 +1,69 @@
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.*;
+import java.io.*;
+
 
 //Given the output of the compiler, and a file, find and output any matching patterns
 public class REsearch {
-    class State {
-        char c;
+    static class State {
+        int stateNum;
+        int c;
         int n1;
         int n2;
 
-        public State(char ch, int n1, int n2) {
-            this.c = c;
+        public State(int stateNum, int ch, int n1, int n2) {
+            this.stateNum = stateNum;
+            this.c = c;     //If char is -1, then that means it is a branching state
             this.n1 = n1;
             this.n2 = n2;
         }
+
+        public int getStateNum() {
+            return stateNum;
+        }
+
+        public int getC() {
+            return c;
+        }
+
+        public int getN1() {
+            return n1;
+        }
+
+        public int getN2() {
+            return n2;
+        }
     }
-    //Deque has addFirst(e), removeFirst(), and addLast(e) eg. "deque.addLast(e)"
-    //Scan state has -2 as both next states so you know that this is the scan
-    State scan = new State('s', -2, -2);
-    Deque<State> deque = new LinkedList<State>();
+
+    public REsearch(String inputFile) {
+        //Deque has addFirst(e), removeFirst(), and addLast(e) eg. "deque.addLast(e)"
+        Deque<State> deque = new LinkedList<State>();
+        //Scan state has -2 as both next states so you know that this is the scan
+        State[] states;
+        State scan = new State(0,0, -2, -2);
+        deque.addFirst(scan);
+
+        try {
+            File file = new File(inputFile);
+            FileInputStream fis = new FileInputStream(file);
+            int r = 0;
+            while((r = fis.read())!=-1)
+            {
+                System.out.print((char)r);      //r is each char read
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String [] args) {
-        System.out.println("Hello World");
+        try {
+            if (args.length == 0) {
+                System.out.println("Usage: java REsearch <file>");
+            }
+            REsearch Rs = new REsearch(args[0]);
+        } catch (Error e) {
+            System.err.println(e);
+        }
     }
 }
