681aafea30cec41cbb675a74e698944a8d04d246
ryangood1 <62087744+ryangood1@users.noreply.github.com>
2020-05-02 13:22:10 +1200

    Add files via upload

--- REGEX/REsearch.java
+++ REGEX/REsearch.java
@@ -4,7 +4,9 @@ import java.io.*;
 
 //Given the output of the compiler, and a file, find and output any matching patterns
 public class REsearch {
-    static class State {
+    List<State> states = new ArrayList<State>();     //Global array to store all the states
+
+    class State {
         int stateNum;
         int c;
         int n1;
@@ -34,11 +36,31 @@ public class REsearch {
         }
     }
 
+    public void getStandardInput() {
+        int state, c, n1, n2;
+        String line;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            line = br.readLine();
+            //System.out.println(line);
+            String[] splitString = line.split(" ");     //Split the input by SOMETHING to get out the state, c, n1, n2
+            state = Integer.parseInt(splitString[0]);
+            c = Integer.parseInt(splitString[1]);
+            n1 = Integer.parseInt(splitString[2]);
+            n2 = Integer.parseInt(splitString[3]);
+            State s = new State(state, c, n1 , n2);
+            states.add(s);      //Add the new state to the array of states
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
     public REsearch(String inputFile) {
         //Deque has addFirst(e), removeFirst(), and addLast(e) eg. "deque.addLast(e)"
+        getStandardInput();
         Deque<State> deque = new LinkedList<State>();
         //Scan state has -2 as both next states so you know that this is the scan
-        State[] states;
         State scan = new State(0,0, -2, -2);
         deque.addFirst(scan);
 
