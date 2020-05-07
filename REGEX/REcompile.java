d9c0a6f93f97ebb26591895d42a7769876a9132c
theDude5 <22064477+theDude5@users.noreply.github.com>
2020-05-07 21:05:04 +1200

    Update REcompile.java

--- REGEX/REcompile.java
+++ REGEX/REcompile.java
@@ -1,3 +1,7 @@
+/**
+ * @author: Stuart Ussher 1060184, 
+ */
+
 public class REcompile {
     private class State {
         int n1, n2, st;
@@ -6,9 +10,10 @@ public class REcompile {
             this.ch = ch;
             this.n1 = n1;
             n2 = n1;
-            this.st = state++;
+            st = state++;
         }
         public void print(){ System.out.println(st+"\t"+ch+"\t"+n1+"\t"+n2); }
+        public void update(int state) { if (n2 == n1){ n2=state; } n1 = state; }
     }
 
     char[] regex;
@@ -17,7 +22,6 @@ public class REcompile {
     public REcompile(String regex){
         this.regex = ('\0'+regex.substring(1, regex.length()-1)).toCharArray();
         states= new State[this.regex.length];
-        //parse();
     }
 
     public void expression(){
@@ -26,34 +30,23 @@ public class REcompile {
     }
 
     public void term(){
-        int r = factor();
-        if (i >= regex.length || !"*?|".contains(regex[i]+"")) { return; }        
-        if (states[state-1].ch != '\0') { states[state] = new State('\0', state+1); }
+        int r = factor(), ref = state;
+        if (i >= regex.length || !"*?|".contains(regex[i]+"")) { return; }
+        states[state] = new State('\0', state+1);
         states[state-1].n2 = r;
-        //r==s
-        if(states[r-1].n2 == states[r-1].n1) {states[r-1].n2=state-1;}
-        states[r-1].n1=state-1;
+        states[r-1].update(state-1);
+        for (int j = 0; j < r; j++) { if(states[j].n1 == r) { states[j].update(state-1); } }
         if (regex[i] == '*') {
-            states[s].n1 = state;
-            states[s].n2 = r < s && states[r+1].ch == '\0' ? r+1 : r;
-            i++;
-        }
-        else if (regex[i] == '?') {
-            if (states[s].n2 == states[s].n1) { states[s].n2 = state; }
             states[s].n1 = state; i++;
-            while (r < state) { 
-                if (states[r].n1 == state-1) { states[r].n1++; }
-                if (states[r].n2 == state-1) { states[r].n2++; }
-                r++;
-            }
+            states[s].n2 = r < s && states[r+1].ch == '\0' ? r+1 : r;
         }
-        else if (regex[i] == '|') {
-            int ref = state-1, t = s; i++;
+        else if (regex[i] == '?') { states[s].update(state); i++; }
+        else {
+            int t = s; i++;
             term();
-            if (states[t].n2 == states[t].n1) { states[t].n2 = state; }
-            states[t].n1 = state;
-            while (r < ref) { if (states[r].n2 == ref) { states[r].n2 = state; } r++; }
+            states[t].update(state);
         }
+        while (r < ref) { if (states[r].n1 == ref) { states[r].update(ref); } r++; }
     }
 
     /**
@@ -63,16 +56,13 @@ public class REcompile {
         if ("*?|".contains(regex[i]+"")) { throw new Error("Misplaced operator"); }
         switch (regex[i]) {
             case '\\':
-                s = state;
-                states[state] = new State(regex[i+1], state+1);
-                i+=2;
-                break;
+                s = state; states[state] = new State(regex[i+1], state+1);
+                i+=2; break;
             case '(':
                 int r = state; i++;
                 expression();
                 if (i >= regex.length || regex[i] != ')') { throw new Error("Missing ')'"); }
-                i++;
-                return r;
+                i++; return r;
             case ')': break;
             // case '.': s = state; states[state] = new State('.', state+1); i++; break;
             default: s = state; states[state] = new State(regex[i], state+1); i++;
@@ -95,8 +85,8 @@ public class REcompile {
     public static void main(String[] args) {
         if (args.length == 0 || !args[0].startsWith("\"") || !args[0].endsWith("\"")) { 
             System.out.println("Usage: java REcompile \\\" <expression> \\\"");
-            args = new String[] {"\""+"a|(jm?b*)|c*"+"\""};
-            // return;
+            // args = new String[] {"\""+"a|(j*m?b(yn)*)|u|c*"+"\""};
+            return;
         }
         REcompile RE = new REcompile(args[0]);
         try { RE.parse(); RE.print(); } catch (Error e) { System.err.println(e); }
