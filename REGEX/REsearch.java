cc4285529a3520e2513288b6f859915441bbac2a
ryangood1 <62087744+ryangood1@users.noreply.github.com>
2020-05-07 16:23:46 +1200

    Add files via upload

--- REGEX/REsearch.java
+++ REGEX/REsearch.java
@@ -2,9 +2,11 @@ import java.util.*;
 import java.io.*;
 
 
-//Given the output of the compiler, and a file, find and output any matching patterns
+//Given the output of the compiler as standard input and a file, find and output any matching patterns
 public class REsearch {
     List<State> states = new ArrayList<State>();     //Global array to store all the states
+    //Deque has addFirst(e), removeFirst(), and addLast(e) eg. "deque.addLast(e)"
+    Deque<Integer> deque = new ArrayDeque<Integer>();    //Deque to facilitate checking of current and next states
 
     class State {
         int stateNum;
@@ -12,11 +14,11 @@ public class REsearch {
         int n1;
         int n2;
 
-        public State(int stateNum, int ch, int n1, int n2) {
-            this.stateNum = stateNum;
-            this.c = c;     //If char is -1, then that means it is a branching state
-            this.n1 = n1;
-            this.n2 = n2;
+        public State(int _stateNum, int _c, int _n1, int _n2) {
+            this.stateNum = _stateNum;
+            this.c = _c;     //If char is \0, then that means it is a branching state
+            this.n1 = _n1;
+            this.n2 = _n2;
         }
 
         public int getStateNum() {
@@ -36,20 +38,30 @@ public class REsearch {
         }
     }
 
+    public State getState(int stateNum) {
+            State s = new State(0, 0, 0, 0);
+            for (int i = 0; i < states.size(); i++) {
+                if (states.get(i).getStateNum() == stateNum) {
+                    s = states.get(i);
+                }
+            }
+        return s;
+    }
+
     public void getStandardInput() {
         int state, c, n1, n2;
         String line;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
             line = br.readLine();
-            //System.out.println(line);
-            String[] splitString = line.split(" ");     //Split the input by SOMETHING to get out the state, c, n1, n2
+            String[] splitString = line.split("\t");     //Split the input by Tab Char to get out the state, c, n1, n2
             state = Integer.parseInt(splitString[0]);
             c = Integer.parseInt(splitString[1]);
             n1 = Integer.parseInt(splitString[2]);
             n2 = Integer.parseInt(splitString[3]);
             State s = new State(state, c, n1 , n2);
             states.add(s);      //Add the new state to the array of states
+            deque.addFirst(s.getStateNum());
         }
         catch (IOException e) {
             System.out.println(e);
@@ -57,20 +69,66 @@ public class REsearch {
     }
 
     public REsearch(String inputFile) {
-        //Deque has addFirst(e), removeFirst(), and addLast(e) eg. "deque.addLast(e)"
+        int condition = 0;
+        //Scan has stateNum of -1, to indicate that it is the state
+        State scan = new State(-1,0, 0, 0);
+        deque.addFirst(scan.getStateNum());
         getStandardInput();
-        Deque<State> deque = new LinkedList<State>();
-        //Scan state has -2 as both next states so you know that this is the scan
-        State scan = new State(0,0, -2, -2);
-        deque.addFirst(scan);
 
         try {
             File file = new File(inputFile);
             FileInputStream fis = new FileInputStream(file);
-            int r = 0;
-            while((r = fis.read())!=-1)
+            int r;          //r is the char being read from the file
+            int p = 0;      //p is the pointer
+            int stateNum = 0;
+            while((r = fis.read()) != -1)
             {
-                System.out.print((char)r);      //r is each char read
+                p = r;
+                Integer currentStateNum = deque.getFirst();
+                deque.removeFirst(); //Pops the state at the top of the deque off
+                State currentState = getState(currentStateNum);
+
+                if(currentState.getC() == -1) {
+                    if(deque.size() > 0) {      //If you popped scan but there are still more states, put it on the botton of deque
+                        State newScan = new State(-1,0, 0, 0);
+                        deque.addLast(newScan.getStateNum());
+                    }
+                    else {      //Otherwise, scan was popped because deque is empty so match has failed
+                        condition = 1;
+                        break;
+                    }
+                }
+                else if(currentState.getC() == r) {     //If the char is a match to our text
+                    if(currentState.getN1() == currentState.getN2()) {        //If n1 is == to n2, only add one of them
+                        deque.addLast(currentState.getN1());
+                    }
+                    else {
+                        deque.addLast(currentState.getN1());
+                        deque.addLast(currentState.getN2());
+                    }
+                    p++;
+                    while(p == r) {       //Moves the pointer along to check the next char if it is the same, and so on until whole pattern is found
+                        System.out.println((char)r);
+                        p++;
+                        r++;
+                        condition = 2;
+                    }
+                }
+                else if(currentState.getC() == '\0') {      //If the char is a Branching State
+                    deque.addFirst(currentState.getN1());
+                    deque.addFirst(currentState.getN2());
+                }
+                //System.out.print((char)r);
+            }
+            fis.close();
+            if(condition == 0) {
+                System.out.println("The End Of The File was reached, and No Matches were found");
+            }
+            if(condition == 1) {
+                System.out.println("No Matches could be found");
+            }
+            if(condition == 2) {
+                System.out.println("A Match has been found");
             }
         }
         catch(Exception e) {
@@ -81,7 +139,7 @@ public class REsearch {
     public static void main(String [] args) {
         try {
             if (args.length == 0) {
-                System.out.println("Usage: java REsearch <file>");
+                System.out.println("Usage: java REsearch <standardInput> <file>");
             }
             REsearch Rs = new REsearch(args[0]);
         } catch (Error e) {
