cd1ee7ae24a30bf7668fcb9c4789ef6dcbbb7b5e
theDude5 <22064477+theDude5@users.noreply.github.com>
2020-06-12 15:38:55 +1200

    Update tinkering.java

--- Assignment4/tinkering.java
+++ Assignment4/tinkering.java
@@ -4,73 +4,111 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import java.util.ArrayList;
 
 public class Counter {
     BufferedImage image;
-    List images;
+    ArrayList<BufferedImage> images;
+    int[][] data;
+    int a_max=0, a_min=0;
+
+    private final int[][] gauss_3 = {{3,5,3}, {5,8,5}, {3,5,3}};
+    private final int[][] gauss_5 = {{0,1,2,1,0}, {1,3,5,3,1}, {2,5,9,5,2}, {1,3,5,3,1}, {0,1,2,1,0}};
+    private final int [][] laplace_3 = {{-1,-1,-1}, {-1,10,-1}, {-1,-1,-1}};
+    private final int [][] laplace_5 = {{0,0,-1,0,0}, {0,-1,-2,-1,0}, {-1,-2,16,-2,-1}, {0,-1,-2,-1,0}, {0,0,-1,0,0}};
+    private final int [][] blur = {{1,1,1},{1,1,1},{1,1,1}};
+    private final int [][] sobel_y = {{1,2,1}, {0,0,0}, {-1,-2,-1}};
+    private final int [][] sobel_x = {{1,0,-1}, {2,0,-2}, {1,0,-1}};
     public Counter(String fileName) {
+        images = new ArrayList<BufferedImage>();
         try {
-            //BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
             image = ImageIO.read(new File(fileName));
-            int[][] data = new int[image.getWidth()][image.getHeight()];
+            data = new int[image.getWidth()][image.getHeight()];
             for (int y = 0; y < image.getHeight(); y++) {
-                for (int x = 0; x < image.getWidth(); x++) {
+                for (int x = 0; x < image.getWidth(); x++) { 
                     data[x][y] = image.getRGB(x, y);
+                    //a_max = Math.max((data[x][y]>>24)&0xff, a_max);
+                    //a_min = Math.max((data[x][y]>>24)&0xff, a_min);
                 }
             }
             //Pipeline goes here
-
-            output(image);
-            output(negative(image));
+            images.add(image);
+            process(image,blur);
+            //gauss(image);
+            //gauss(image);
+            output();
         }
-        catch (IOException e) {
-            String workingDir = System.getProperty("user.dir");
-            System.out.println("Current working directory : " + workingDir);
-            //e.printStackTrace();
-        }
+        catch (IOException e) { System.err.println(e); }
+    }
+    
+    private void process(BufferedImage img, int[][] mask) {
+        int s = (int) Math.floor(mask.length/2);
+        for (int y = s; y < image.getHeight()-s; y++) {
+            for (int x = s; x < image.getWidth()-s; x++) {
+                apply_filter(x, y, mask);
+            }
+        } images.add(apply());
     }
 
-    private BufferedImage negative(BufferedImage img) {
+    private void apply_filter(int x, int y, int[][] mask){
+        int p=0, a=0, r=0, b=0, g=0,n=0, m;
+        for (int j = 0; j < mask.length; j++) {
+           for (int i = 0; i < mask.length; i++) {
+                m = mask[i][j];
+                if (m == 0) { continue; }
+                p = data[x+i-(int) Math.floor(mask.length/2)][y+j-(int) Math.floor(mask.length/2)];
+                a += m*(p>>24)&0xff;
+                r += m*(p>>16)&0xff;
+                g += m*(p>>8)&0xff;
+                b += m*p&0xff;
+                n++;
+                //n+=Math.abs(m);
+            }
+        }
+        //a=Math.abs(a)/n; r=Math.abs(r)/n; g=Math.abs(g)/n; b=Math.abs(b)/n;
+        a =Math.min(255, Math.max(0,a/n)); r =Math.min(255, Math.max(0,r/n)); g=Math.min(255, Math.max(0,g/n)); b=Math.min(255, Math.max(0,b/n));
+        data[x][y] = ((a<<24) | (r<<16) | (g<<8) | b); 
+    }
+
+    private void negative(BufferedImage img) {
         int p,a,r,b,g;
-        for (int i = 0; i < image.getHeight(); i++) {
-            for (int j = 0; j < image.getWidth(); j++) {
-                p = image.getRGB(j,i); 
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {        
+                p = data[x][y];
                 a = (p>>24)&0xff; 
                 r = (p>>16)&0xff; 
                 g = (p>>8)&0xff; 
-                b = p&0xff; 
+                b = p&0xff;
                 
-                //subtract RGB from 255 
                 r = 255 - r; 
                 g = 255 - g; 
                 b = 255 - b;
-
-                //set new RGB value 
-                p = (a<<24) | (r<<16) | (g<<8) | b; 
-                image.setRGB(j, i, p); 
+                data[x][y] = (a<<24) | (r<<16) | (g<<8) | b;
             }
-        }
-        return img;
+        } images.add(apply());
     }
-    public void output(BufferedImage image) {
+
+    private BufferedImage apply() {
+        BufferedImage img = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
+        for (int y = 0; y < img.getHeight(); y++) {
+            for (int x = 0; x < img.getWidth(); x++) { img.setRGB(x, y, data[x][y]); }
+        } return img;
+    }
+
+    public void output() {
         JFrame frame = new JFrame("Cell Image");
         JPanel panel = new JPanel();
-        JLabel label = new JLabel();
-        ImageIcon img = new ImageIcon(image);
-        label.setIcon(img);
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
-        panel.setLayout(new GridLayout(0,1));
-        panel.add(label);
-        frame.add(panel, BorderLayout.CENTER);
-
-        frame.pack();
-        frame.setTitle("Counter");
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        panel.setLayout(new GridLayout(images.size(),1,10,10));        
+        for (BufferedImage item : images) { panel.add(new JLabel( new ImageIcon(item))); }
+        frame.add(new JScrollPane(panel), BorderLayout.CENTER);
+        frame.setBounds(0, 0, image.getWidth()+100, image.getHeight()+75);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
@@ -79,7 +117,7 @@ public class Counter {
     public static void main(String [] args) {
         if (args.length == 0) {
             System.out.println("Usage: java Counter <imagefile.jpg>");
-            args = new String[]{"45762.jpg"};
+            args = new String[]{"test.jpg"};
         }
         File file = new File(args[0]);
         if (file.exists()) {new Counter(args[0]);}
